05b215ff92005e562cf4534e53c2ad96fde0232a
Alvin Quach <alvinwquach@gmail.com>
2022-12-02 17:44:00 -0800

    Create footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Footer from "../components/Footer";
 import Navbar from "../components/global/Navbar";
 
 const Home: NextPage = () => {
   return (
-    <div className="min-h-screen">
-      <Navbar />
-      <Head>
-        <title>Shift Into Your Closet</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
+    <>
+      <div className="min-h-screen">
+        <Head>
+          <title>Shift Into Your Closet</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Navbar />
+      </div>
+      <Footer />
+    </>
   );
 };
 

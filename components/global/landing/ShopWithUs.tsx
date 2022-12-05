feb4b7648b772b4b6c39204fe3f682680ab87556
Alvin Quach <alvinwquach@gmail.com>
2022-12-05 12:08:34 -0800

    Create shop with us

---
+++ components/global/landing/ShopWithUs.tsx
@@ -0,0 +1,54 @@
+function ShopWithUs() {
+  return (
+    <>
+      <section className="max-w-7xl mx-auto mt-5 px-5 sm:px-6 lg:px-8 py-20 sm:py-24 lg:py-24  dark:text-white min-h-screen bg-zinc-800">
+        <h3 className="text-3xl tracking-widest mb-3 uppercase text-gray-400 font-bold ">
+          Shop With Us
+        </h3>
+        <p className="mt-3 sm:mt-4 text-xl text-gray-200 font-light">
+          Our vision has always been to create a marketplace for everyone. We
+          are committed to bridging the gap between the consumer and the
+          product.
+        </p>
+        <h4 className="mt-10 font-bold uppercase tracking-wide text-2xl bg-gradient-to-r from-blue-400 via-teal-600 to-blue-600 bg-clip-text fill-transparent [-webkit-text-fill-color:transparent]">
+          Why Customers Choose Us
+        </h4>
+        <div className="mt-8 text-xl font-bold sm:text-2xl text-white">
+          ✅ Customer Satisfaction
+        </div>
+        <p className="mt-3 sm:mt-4 text-xl text-gray-200 font-light">
+          We strive to be able to provide the best customer experience possible.
+          We will provide additional pictures upon request to ensure that our
+          customers are receiving exactly what they see. We aim to provide great
+          customer support with a 24 hour turnaround time on any and all
+          inquiries.
+        </p>
+        <div className="mt-8 text-xl font-bold sm:text-2xl text-white">
+          ✅ Authenticity Guaranteed
+        </div>
+        <p className="mt-3 sm:mt-4 text-xl text-gray-200 font-light">
+          All of our products are 100% authentic. Please purchase with complete
+          confidence.
+        </p>
+        <div className="mt-8 text-xl font-bold sm:text-2xl text-white">
+          ✅ Inventory Refresh
+        </div>
+        <p className="mt-3 sm:mt-4 text-xl text-gray-200 font-light">
+          We know how frustrating it can be to receive an email that your order
+          has been cancelled due to an item being out of stock. Please rest
+          assured that our inventory will reflect what is currently available.
+          Our inventory is updated as items sell out.
+        </p>
+        <div className="mt-8 text-xl font-bold sm:text-2xl text-white">
+          ✅ Extensive Selection
+        </div>
+        <p className="mt-3 sm:mt-4 text-xl text-gray-200 font-light">
+          Our products are curated worldwide. We offer a wide variety of brands
+          ranging from designer to streetwear and more!
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default ShopWithUs;

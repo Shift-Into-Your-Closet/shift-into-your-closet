28f92acc5df9f5662b52f5c8ae88955aacf5823a
Alvin Quach <alvinwquach@gmail.com>
2022-12-10 20:32:03 -0800

    Refactor product pages into array

--- components/ui/Breadcrumbs.tsx
+++ components/ui/Breadcrumbs.tsx
@@ -21,6 +21,8 @@ function Breadcrumbs() {
     const isShippingReturnsPage = shippingReturnsRegex.test(router.pathname);
     const isHyphenatedPage = hyphenatedPageRegex.test(router.pathname);
 
+    const isProductPage = [isShoePage, isApparelPage, isAccessoryPage];
+
     switch (true) {
       case isShoePage || isAccessoryPage:
         pageName = pageName
@@ -74,7 +76,7 @@ function Breadcrumbs() {
             </Link>
           </li>
           <li aria-current="page" className="inline-flex items-center">
-            {isShoePage || isApparelPage || isAccessoryPage ? (
+            {isProductPage ? (
               <Link
                 href={pagePath}
                 className="inline-flex items-center text-lg font-medium md:ml-2 cursor-pointer hover:text-blue-400"
@@ -116,23 +118,6 @@ function Breadcrumbs() {
                 </span>
               </div>
             )}
-            <div className="flex items-center">
-              <svg
-                className="w-6 h-6 text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span className="ml-1 text-lg font-medium md:ml-2">
-                {pageName}
-              </span>
-            </div>
           </li>
         </ol>
       </nav>

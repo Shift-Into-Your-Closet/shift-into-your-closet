6425eb33dd1c0b3dd79f98fde89e84ca52bc75f4
Alvin Quach <alvinwquach@gmail.com>
2022-12-14 02:13:45 -0800

    Update about us page

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,9 +1,10 @@
 import { Disclosure, Transition } from "@headlessui/react";
+
 import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { HiChevronRight, HiChevronUp, HiPlus } from "react-icons/hi";
-import faqs from "../utils/data/faqs";
+
+import { HiChevronRight } from "react-icons/hi";
 
 import cn from "clsx";
 
@@ -20,6 +21,41 @@ function About({ title, children }: AboutProps) {
     </>
   );
 }
+
+const faqs = [
+  {
+    id: 1,
+    question: "How can I purchase?",
+    answer:
+      "We are an email to order service. Emails will be responded to in the order that they are received. We will do our best to provide a reply ASAP. Item(s) will be removed from our site as the item or size sells out. Please expect a delayed response during observed holidays. We are located in the Bay Area (PDT) and our response time may vary in different time zones.",
+  },
+  {
+    id: 2,
+    question: "What if there are several products that I want to purchase?",
+    answer:
+      "If there are several products that you're interested in, please let us know.",
+  },
+  {
+    id: 3,
+    question:
+      "I understand that items come from multiple sellers. How can I be sure that what I'm receiving is authentic?",
+    answer:
+      "Every item is inspected by our experienced team for authenticity. All inventory is from trusted suppliers. Please purchase with complete confidence. We will provide additional pictures upon request. We want to make sure that you're seeing exactly what you'll be receiving.",
+  },
+  {
+    id: 4,
+    question: "Will my order be cancelled due to an item being out of stock?",
+    answer:
+      "We know how frustrating it can be to receive an email that your order has been cancelled due to an item being out of stock. Please rest assured that you will recieve your order. Our inventory will reflect what is currently available. We update our online store frequently as items sell out.",
+  },
+  {
+    id: 5,
+    question:
+      "There's an item that I'm interested in. I was wondering if you accept offers?",
+    answer:
+      "There are several items on our online store where the seller will accept an offer. Please feel free to send an offer.",
+  },
+];
 
 const AboutUs: NextPage = () => {
   return (
@@ -105,6 +141,8 @@ const AboutUs: NextPage = () => {
             general public.
           </p>
         </About>
+        <hr className="my-10" />
+        <h4 className="text-4xl text-center">F.A.Q</h4>
         <p className="text-center text-xl mt-10">
           (Frequently Asked Questions)
         </p>

2300c32d76bf66833956228e1bc13f14109512e9
Alvin Quach <alvinwquach@gmail.com>
2023-05-05 15:39:13 -0700

    Create custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <section className="max-w-7xl mx-auto mt-5 px-5 sm:px-6 lg:px-8 py-20 sm:py-24 lg:py-24 font-roboto min-h-screen">
+      <h1 className="text-7xl text-white text-center">404 - Page Not Found</h1>
+    </section>
+  );
+}
